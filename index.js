973226b3c5773d4360c42a800f187126bf231074
Sanja <sanjazitnik555@gmail.com>
2025-10-28 11:51:13 +0100

    Samostalni zadatak za vježbu 1

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+const korisnici=[{id:1, ime:'David', prezime:'Johnson'},
+    {id:2, ime:'Moki', prezime:'Bubic'},
+    {id:3, ime:'Bibi', prezime:'Jarkov'}
+]
+
 const express = require('express');
+const path=require('path');
 const app = express();
 const PORT = 3000;
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-res.send('Hello, world!');
+res.sendFile( 'index.html', {root:'public'});
 });
+
+app.get('/about', (req, res) => {
+res.sendFile('about.html', {root:'public'});
+});
+
+app.get('/users', (req, res) => {
+    console.log("podaci: ", korisnici)
+res.json(korisnici);
+});
+
 app.listen(PORT, error => {
 if (error) {
 console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
